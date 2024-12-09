eb36b8a9b248f5ddf5fede9dc5748be56452624b
gautam <gautamuttam0001@gmail.com>
2024-12-09 14:04:29 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,7 @@ package com.example;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, GitHub Actions!");
+        System.out.println("Hello, World!");
     }
 }
+
